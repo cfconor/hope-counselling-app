90adcbe6ea0d91dbf5517603b56d909b4d11f8ab
cfconor <cfconor@gmail.com>
2021-08-02 14:05:40 +0000

    fixed project folder structure, made init migration

--- hope_counselling/settings.py
+++ hope_counselling/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bsc&%a$t4=n_l(#3v1m%)tei61^#x+^r)it%zphb&)9yjhai+#'
+SECRET_KEY = 'django-insecure-9+_xjl@*6j039e$z9*cc1jj97b@)ei87k5j(5im%o6g3y%7ejd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
